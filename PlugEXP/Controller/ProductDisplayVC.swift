1dad85983469a022fa0d0c613ff537f28dac2ea4
Bilal <admin@admins-MBP.T-mobile.com>
2017-11-14 15:36:18 +0500

    Login ui completed for iphone

---
+++ PlugEXP/Controller/ProductDisplayVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProductDisplayVC.swift
+//  PlugEXP
+//
+//  Created by admin on 11/14/17.
+//  Copyright © 2017 brainplow. All rights reserved.
+//
+
+import UIKit
+
+class ProductDisplayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}
